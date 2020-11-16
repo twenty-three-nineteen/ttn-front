670f873d01b6e95d2f697b4249dcbb4d59c87718
hasti <hastikaramdel@gmail.com>
2020-11-16 20:58:20 +0330

    app jsx

--- Tinder2319/src/App.jsx
+++ Tinder2319/src/App.jsx
@@ -5,14 +5,19 @@ import history from "./core/modules/history";
 import "antd/dist/antd.css";
 import Profile from "./views/pages/Profile/Profile";
 import LoginSignUp from "./views/pages/LoginSignUp";
+import Posts from "./views/pages/Posts/Posts";
+import ShowPost from "./views/pages/ShowPost/ShowPost";
+
 
 
 const App = () => {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/LoginSignUp" exact component={LoginSignUp} />
-        <Route path="/Profile" exact component={Profile} />
+        <Route path="/login_signup" exact component={LoginSignUp} />
+        <Route path="/profile" exact component={Profile} />
+        <Route path="/posts" exact component={Posts} />
+        <Route path="/show_post" exact component={ShowPost} />
       </Switch>
     </Router>
   );
