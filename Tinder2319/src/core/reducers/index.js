5850b6d9b42a29ef994c2c266b4c2b34cc78b552
hasti <hastikaramdel@gmail.com>
2020-11-16 20:58:53 +0330

    reducers

--- Tinder2319/src/core/reducers/index.js
+++ Tinder2319/src/core/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import profile from "../profile/reducer/profile.js";
+import posts from "../profile/reducer/posts.js";
 
 export default combineReducers({
-    profile,
+    profile,posts
 });
