99eb4ad4f6ea54eeccee83c940cc21efccddce69
unknown <lotfolahi.y@gmail.com>
2020-11-16 19:32:48 +0330

    combine reducers

--- Tinder2319/src/core/reducers/index.js
+++ Tinder2319/src/core/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from "redux";
 import profile from "../profile/reducer/profile.js";
+import email_confirmation from "../email-confirmation/reducer/emailConfirmationReducer";
+import create_profile from "../create-profile/reducer/createProfileReducer";
+import login_signup from "../login-signup/reducer/loginSignupReducer";
 
 export default combineReducers({
     profile,
+    create_profile,
+    email_confirmation,
+    login_signup,
 });
