e7a0e535ffaa9f1bd238a07a2e162f407f6a7a6b
unknown <lotfolahi.y@gmail.com>
2020-11-16 19:30:25 +0330

    login signup redux

---
+++ Tinder2319/src/core/login-signup/action/loginSignupActions.js
@@ -0,0 +1,80 @@
+import {ActionTypes} from "./loginSignupActionTypes"
+
+
+export function setFormState(state){
+    return {
+        type : ActionTypes.SET_FORM_STATE,
+        payload: { state },
+    };
+}
+
+export function setUsername(username){
+    return {
+        type : ActionTypes.SET_USERNAME,
+        payload: { username },
+    };
+}
+
+export function setPassword(password){
+    return {
+        type : ActionTypes.SET_PASSWORD,
+        payload: { password },
+    };
+}
+
+
+export function setEmail(email){
+    return {
+        type : ActionTypes.SET_EMAIL,
+        payload: { email },
+    };
+}
+
+export function setToken(token){
+    return {
+        type : ActionTypes.SET_TOKEN,
+        payload: { token },
+    };
+}
+
+export function setLoading(loading){
+    return {
+        type : ActionTypes.SET_LOADING,
+        payload: { loading },
+    };
+}
+
+export function setForgotPasswordModal(visible){
+    return {
+        type : ActionTypes.SET_FORGOT_PASS_MODAL,
+        payload: { visible },
+    };
+}
+
+export function setSignUpState(success){
+    return {
+        type : ActionTypes.SET_SIGNUP_STATE,
+        payload: { success },
+    };
+}
+
+export function setLoginState(success){
+    return {
+        type : ActionTypes.SET_LOGIN_STATE,
+        payload: { success },
+    };
+}
+
+export function setForgotPassState(success){
+    return {
+        type : ActionTypes.SET_FORGOT_PASS_STATE,
+        payload: { success },
+    };
+}
+
+export function setSignUpModal(visible){
+    return {
+        type : ActionTypes.SET_SIGNUP_MODAL,
+        payload: { visible },
+    };
+}
---
+++ Tinder2319/src/core/login-signup/reducer/loginSignupReducer.js
@@ -0,0 +1,95 @@
+import {ActionTypes} from "../action/loginSignupActionTypes";
+const initialstate = { 
+  formState: 1,
+  username: undefined,
+  password: undefined,
+  email: undefined,
+  forgotPasswordToggle: false,
+  token: undefined,
+  loading: false,
+  s_visible:false,
+  f_visible:false,
+  signup_success:false,
+  forgot_pass_success:false,
+  login_success:false,
+
+
+ 
+  
+}
+export default (state = initialstate, { type, payload }) => {
+    switch (type) {
+     
+      case ActionTypes.SET_FORM_STATE:
+        return{
+          ...state, 
+          formState : ((payload.state>3)? state.formState : payload.state),
+      };
+
+      case ActionTypes.SET_USERNAME:
+        return{
+          ...state, 
+          username : payload.username,
+      };
+
+
+      case ActionTypes.SET_PASSWORD:
+        return{
+          ...state, 
+          password : payload.password,
+      };
+
+
+      case ActionTypes.SET_EMAIL:
+        return{
+          ...state, 
+          email : payload.email,
+      };
+
+      case ActionTypes.SET_TOKEN:
+        return{
+          ...state, 
+          token : payload.token,
+      };
+
+      case ActionTypes.SET_LOADING:
+        return{
+          ...state, 
+          loading : payload.loading,
+      };
+
+      case ActionTypes.SET_FORGOT_PASS_MODAL:
+        return{
+          ...state, 
+          f_visible : payload.visible,
+      };
+
+      case ActionTypes.SET_SIGNUP_MODAL:
+        return{
+          ...state, 
+          s_visible : payload.visible,
+      };
+
+      case ActionTypes.SET_SIGNUP_STATE:
+        return{
+          ...state, 
+          signup_success : payload.success,
+      };
+
+      case ActionTypes.SET_LOGIN_STATE:
+        return{
+          ...state, 
+          login_success : payload.success,
+      };
+
+      case ActionTypes.SET_FORGOT_PASS_STATE:
+        return{
+          ...state, 
+          forgot_pass_success : payload.success,
+      };
+      
+      
+    default : return state;
+
+    }
+}
