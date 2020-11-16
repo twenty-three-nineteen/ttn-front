41dea59735a7150092849fc1d6fa1706401949bf
unknown <lotfolahi.y@gmail.com>
2020-11-16 19:31:20 +0330

    login signup layout and components

--- Tinder2319/src/views/pages/LoginSignUp/LoginSignUp.js
+++ Tinder2319/src/views/pages/LoginSignUp/LoginSignUp.js
@@ -1,21 +1,21 @@
 import React from 'react';
-// import LoginForm from '../../components/LoginForm';
-// import SignUpForm from '../../components/SignUpForm';
-import { Tabs,Menu,Popover, Button } from 'antd';
-import '../styles/App.css';
+import LoginSignUpForm from './components/LoginSignUpForm';
+import '../../styles/App.css';
+import '../../styles/LoginSignUp.css';
 
-const { TabPane } = Tabs;
+import { Tooltip  } from 'antd';
+
+import {connect} from 'react-redux';
+import * as login_signup_actions from '../../../core/login-signup/action/loginSignupActions';
 
 function callback(key) {
   console.log(key);
 }
-const content = (
-  <LoginForm/>
-);
+
 const appName = "app name";
 
-export default class LoginSignUp extends React.Component {
-  render() {
+const LoginSignUp = ({formState, setFormState}) =>  {
+
     return (
       <div className="login-signup-container">
   <div className='box'>
@@ -25,12 +25,45 @@ export default class LoginSignUp extends React.Component {
   </div>
   
   <div id="form-container">
-  <h1>{appName}</h1>
-  <LoginForm/>
- 
-</div>
+  <h1 id="state-row">
+  <Tooltip title="Forgot your password?">
+  <button
+  onClick={()=>setFormState(0)}
+  className={(formState==0)? "checked":""}
+  >Reset</button>
+  </Tooltip>
 
+  <Tooltip title="Already have an account?">
+  <button
+  onClick={()=>setFormState(1)}
+  className={(formState==1)? "checked":""}
+  >Login</button>
+  </Tooltip>
+
+  
+  <Tooltip title="Don't have an account?">
+  <button
+  onClick={()=>setFormState(2)}
+  className={(formState==2)? "checked":""}
+  >Sign Up</button>
+  </Tooltip>
+
+  </h1>
+  <LoginSignUpForm/>
+</div>
     </div>
     );
   }
-}
+
+  const mapStateToProps = (state) =>{
+    return{
+      formState: state.login_signup.formState,
+    }
+  } 
+  const mapDispatchToProps = (dispatch) => {
+    return{
+      setFormState: (s) => dispatch(login_signup_actions.setFormState(s)),
+    }
+  }
+  
+  export default connect(mapStateToProps,mapDispatchToProps)(LoginSignUp);
