24761b326fb0f68d8a7204311704cda735243eb5
Y L <lotfolahi.y@gmail.com>
2021-02-04 22:45:41 +0330

    all avatars added

--- Tinder2319/src/views/pages/CreateProfile/components/Avatar.js
+++ Tinder2319/src/views/pages/CreateProfile/components/Avatar.js
@@ -18,6 +18,46 @@ import av14 from '../../../../assessts/images/avatars/av (14).png';
 import av15 from '../../../../assessts/images/avatars/av (15).png';
 
 import av16 from '../../../../assessts/images/avatars/av (16).png';
+import av17 from '../../../../assessts/images/avatars/av (17).png';
+import av18 from '../../../../assessts/images/avatars/av (18).png';
+import av19 from '../../../../assessts/images/avatars/av (19).png';
+import av20 from '../../../../assessts/images/avatars/av (20).png';
+
+import av21 from '../../../../assessts/images/avatars/av (21).png';
+import av22 from '../../../../assessts/images/avatars/av (22).png';
+import av23 from '../../../../assessts/images/avatars/av (23).png';
+import av24 from '../../../../assessts/images/avatars/av (24).png';
+import av25 from '../../../../assessts/images/avatars/av (25).png';
+
+import av26 from '../../../../assessts/images/avatars/av (26).png';
+import av27 from '../../../../assessts/images/avatars/av (27).png';
+import av28 from '../../../../assessts/images/avatars/av (28).png';
+import av29 from '../../../../assessts/images/avatars/av (29).png';
+import av30 from '../../../../assessts/images/avatars/av (30).png';
+
+import av31 from '../../../../assessts/images/avatars/av (31).png';
+import av32 from '../../../../assessts/images/avatars/av (32).png';
+import av33 from '../../../../assessts/images/avatars/av (33).png';
+import av34 from '../../../../assessts/images/avatars/av (34).png';
+import av35 from '../../../../assessts/images/avatars/av (35).png';
+import av36 from '../../../../assessts/images/avatars/av (36).png';
+import av37 from '../../../../assessts/images/avatars/av (37).png';
+import av38 from '../../../../assessts/images/avatars/av (38).png';
+import av39 from '../../../../assessts/images/avatars/av (39).png';
+import av40 from '../../../../assessts/images/avatars/av (40).png';
+
+import av41 from '../../../../assessts/images/avatars/av (41).png';
+import av42 from '../../../../assessts/images/avatars/av (42).png';
+import av43 from '../../../../assessts/images/avatars/av (43).png';
+import av44 from '../../../../assessts/images/avatars/av (44).png';
+import av45 from '../../../../assessts/images/avatars/av (45).png';
+import av46 from '../../../../assessts/images/avatars/av (46).png';
+import av47 from '../../../../assessts/images/avatars/av (47).png';
+import av48 from '../../../../assessts/images/avatars/av (48).png';
+import av49 from '../../../../assessts/images/avatars/av (49).png';
+import av50 from '../../../../assessts/images/avatars/av (50).png';
+
+
 export default [
   av1,
   av2,
@@ -35,5 +75,39 @@ export default [
   av14,
   av15,
   av16,
+  av17,
+av18,
+av19,
+av20,
+av21,
+av22,
+av23,
+av24,
+av25,
+av26,
+av27,
+av28,
+av29,
+av30,
+av31,
+av32,
+av33,
+av34,
+av35,
+av36,
+av37,
+av38,
+av39,
+av40,
+av41,
+av42,
+av43,
+av44,
+av45,
+av46,
+av47,
+av48,
+av49,
+av50,
 ]
 
